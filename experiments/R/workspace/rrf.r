6716e2c67a43f0a51974896b5fd09ec5f33ec892
rrrrind <masaya.mori0905@gmail.com>
2022-01-19 01:50:15 +0900

    Comp till modularization

--- experiments/R/workspace/rrf.r
+++ experiments/R/workspace/rrf.r
@@ -3,44 +3,47 @@ library(RRF)
 # 乱数の固定
 set.seed(1)
 
-# pythonからもらうのは
-# ntree, nodesize
+# --------------------------------------------------------------------------
+# Python側から
+# X_TRAIN, Y_TRAIN, X_VAL, Y_VAL, X_TEST, NTREE, NODESIZEを入力してもらう
+# R側からはReturnとして
+# X_TRAINの予測値, X_VALの予測値, X_TESTの予測値, 重要な特徴量, 特徴量重要度
+# を返す
+# --------------------------------------------------------------------------
 
-# python(pandas)からデータをもらったと仮定する
-# (200,10)のデータフレーム
-x_train <- data.frame(
-  "feature1" = seq(1,100000,length=200), "feature2" = seq(1,50000,length=200),
-  "feature3" = seq(1,10000,length=200), "feature4" = seq(1,5000,length=200),
-  "feature5" = seq(1,1000,length=200), "feature6" = seq(1,500,length=200),
-  "feature7" = seq(1,100,length=200), "feature8" = seq(1,50,length=200),
-  "feature9" = seq(1,10,length=200), "feature10" = seq(1,5,length=200)
-)
-# (20,10)のデータフレーム
-x_test <- data.frame(
-  "feature1" = seq(1,100000,length=20), "feature2" = seq(1,50000,length=20),
-  "feature3" = seq(1,10000,length=20), "feature4" = seq(1,5000,length=20),
-  "feature5" = seq(1,1000,length=20), "feature6" = seq(1,500,length=20),
-  "feature7" = seq(1,100,length=20), "feature8" = seq(1,50,length=20),
-  "feature9" = seq(1,10,length=20), "feature10" = seq(1,5,length=20)
-)
+main <- function(){
+  rrf <- build_rrf(X_TRAIN, Y_TRAIN, X_VAL, Y_VAL, NTREE, NODESIZE)
+  rrf_res <- get_rrf_res(rrf, X_VAL, X_TEST)
+  return(rrf_res)
+}
 
-y_train <- rnorm(200)
-y_test <- rnorm(200)
+build_rrf <- function(x_train, y_train, x_val, y_val, ntree, nodesize){
+  # rrfの構築と学習
+  # パラメータについては次を参照
+  # https://www.rdocumentation.org/packages/RRF/versions/1.9.1/topics/RRF
+  rrf <- RRF(
+    x= x_train, y= y_train, xtest= x_val, ytest= y_val, 
+    ntree= ntree, nodesize= nodesize, importance= TRUE,
+    do.trace= TRUE, keep.forest= TRUE, corr.bias= FALSE, flagReg= 1)
+  
+  # 学習済みモデルを返す
+  return(rrf)
+}
 
+get_rrf_res <- function(rrf, x_val, x_test){
+  # 学習データにおける予測値
+  pred_train <- rrf$predicted
+  # バリデーションデータにおける予測値
+  pred_val <- predict(rrf, x_val)
+  # テストデータにおける予測値
+  pred_test <- predict(rrf, x_test)
+  # 重要な特徴量
+  imp_feat = rrf$feaSet - 1 # pythonとindex番号が違う
+  # 特徴量重要度
+  imp_feat_val = importance(rrf)
+  
+  return(list(pred_train, pred_val, pred_test, imp_feat, imp_feat_val))
+}
 
-# rrfの適用
-rrf <- RRF(
-  x=x_train, y=y_train,
-  ntree= 500, nodesize=5, importance=TRUE, do.trace=TRUE, corr.bias=FALSE, flagReg = 1)
-
-
-rrf$feaSet
-importance(rrf)
-varImpPlot(rrf)
-
-predict(rrf, x_test)
-
-
-
-
-rrf$mse
+# main関数
+main()
