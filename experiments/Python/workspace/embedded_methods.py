6e0b6e0c58d60d8edf4cdb0852a00500584bf74c
rrrrind <masaya.mori0905@gmail.com>
2022-01-19 19:47:01 +0900

    Add Docstring

--- experiments/Python/workspace/embedded_methods.py
+++ experiments/Python/workspace/embedded_methods.py
@@ -3,6 +3,28 @@ import pandas as pd
 import pyper
 
 class regularized_random_forest:
+    """docstring
+    About：
+        Rの『Regularized Random Forest』パッケージを
+        Pythonで使えるようにした
+        
+    How to use:
+        from embedded_methods import regularized_random_forest as RRF
+        model = RRF(ntree=600, nodesize=10)
+        result = model.fit_pred(x_train, y_train, x_val, x_test)
+        
+    Input:
+        x_train, x_val, x_test(pd.DataFrame) : 学習/バリデーション/テストデータの説明変数
+        y_train(np.array) : 学習データの目的変数
+        ntree, nodesize : RRFのパラメータ
+        
+    Output:
+        result[0] : 学習データにおける予測値
+        result[1] : バリデーションデータにおける予測値
+        result[2] : テストデータにおける予測値
+        result[3] : 重要な特徴量
+        result[4] : 特徴量重要度
+    """
     def __init__(self, ntree, nodesize):
         self.ntree = ntree
         self.nodesize = nodesize
@@ -11,8 +33,6 @@ class regularized_random_forest:
         r = self._set_r_params(x_train, y_train, x_val, x_test)
         self._fit(r)
         result = self._predict(r)
-    
-        
         return result
         
     def _set_r_params(self, x_train, y_train, x_val, x_test):
